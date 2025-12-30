69eac26e0e82b024910bc2578c5a244f4214a356
Filip <fwchelkowski@gmail.com>
2025-12-30 10:34:38 +0100

    Task finished. mvn test failed due to checkstyle execution.

--- src/main/java/core/basesyntax/Robot.java
+++ src/main/java/core/basesyntax/Robot.java
@@ -1,14 +1,16 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class Robot {
-    private Direction direction;
     private int coordinateX;
     private int coordinateY;
+    private Direction direction;
 
-    public Robot(Direction direction, int coordinateX, int coordinateY) {
-        this.direction = direction;
-        this.coordinateX = coordinateX;
-        this.coordinateY = coordinateY;
+    public Robot() {
+        this.coordinateX = new Random().nextInt();
+        this.coordinateY = new Random().nextInt();
+        this.direction = Direction.values()[new Random().nextInt(Direction.values().length)];
     }
 
     public Direction getDirection() {
@@ -28,11 +30,11 @@ public class Robot {
             case UP:
                 direction = Direction.LEFT;
                 break;
-            case LEFT:
-                direction = Direction.DOWN;
-                break;
             case DOWN:
                 direction = Direction.RIGHT;
+                break;
+            case LEFT:
+                direction = Direction.DOWN;
                 break;
             case RIGHT:
                 direction = Direction.UP;
@@ -47,11 +49,11 @@ public class Robot {
             case UP:
                 direction = Direction.RIGHT;
                 break;
-            case LEFT:
-                direction = Direction.UP;
-                break;
             case DOWN:
                 direction = Direction.LEFT;
+                break;
+            case LEFT:
+                direction = Direction.UP;
                 break;
             case RIGHT:
                 direction = Direction.DOWN;
@@ -66,11 +68,11 @@ public class Robot {
             case UP:
                 coordinateY++;
                 break;
-            case LEFT:
-                coordinateX--;
-                break;
             case DOWN:
                 coordinateY--;
+                break;
+            case LEFT:
+                coordinateX--;
                 break;
             case RIGHT:
                 coordinateX++;
--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,45 @@
 package core.basesyntax;
 
+import static core.basesyntax.Direction.*;
+
 public class RobotRoute {
+
+    public RobotRoute() {
+
+    }
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() < toX) {
+            while (robot.getDirection() != RIGHT) {
+                robot.turnRight();
+            }
+            while (robot.getX() < toX) {
+                robot.stepForward();
+            }
+        } else if (robot.getX() > toX) {
+            while (robot.getDirection() != LEFT) {
+                robot.turnLeft();
+            }
+            while (robot.getX() > toX) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getY() < toY) {
+            while (robot.getDirection() != UP) {
+                robot.turnRight();
+            }
+            while (robot.getY() < toY) {
+                robot.stepForward();
+            }
+        } else if (robot.getY() > toY) {
+            while (robot.getDirection() != DOWN) {
+                robot.turnLeft();
+            }
+            while (robot.getX() > toX) {
+                robot.stepForward();
+            }
+        }
+
     }
 }
